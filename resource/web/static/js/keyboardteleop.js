b69b45d7a61352d37fe9a2b1a36d3f70cc8cf8b0
Nikolay Ivanov <57194638+NikolayIvanovWS@users.noreply.github.com>
2025-11-28 18:29:53 +0300

    Update keyboardteleop.js

--- resource/web/static/js/keyboardteleop.js
+++ resource/web/static/js/keyboardteleop.js
@@ -1,124 +1,53 @@
-/**
- * @author Russell Toris - rctoris@wpi.edu
- */
-
 var KEYBOARDTELEOP = KEYBOARDTELEOP || {
   REVISION : '0.4.0-SNAPSHOT'
 };
 
-/**
- * @author Russell Toris - rctoris@wpi.edu
- */
-
-/**
- * Manages connection to the server and all interactions with ROS.
- *
- * Emits the following events:
- *   * 'change' - emitted with a change in speed occurs
- *
- * @constructor
- * @param options - possible keys include:
- *   * ros - the ROSLIB.Ros connection handle
- *   * topic (optional) - the Twist topic to publish to, like '/cmd_vel'
- *   * throttle (optional) - a constant throttle for the speed
- */
 KEYBOARDTELEOP.Teleop = function(options) {
   var that = this;
   options = options || {};
   var ros = options.ros;
   var topic = options.topic || '/cmd_vel';
-  // permanent throttle
   var throttle = options.throttle || 1.0;
 
-  // used to externally throttle the speed (e.g., from a slider)
   this.scale = 1.0;
 
-  // linear x and y movement and angular z movement
-  var x = 0;
-  var y = 0;
-  var z = 0;
+  var x = 0, y = 0, z = 0; // текущие команды движения
 
   var cmdVel = new ROSLIB.Topic({
     ros : ros,
     name : topic,
     messageType : 'geometry_msgs/msg/Twist',
-    queue_length: 20
+    queue_length: 10
   });
 
-  // sets up a key listener on the page used for keyboard teleoperation
-  var handleKey = function(keyCode, keyDown) {
-    // used to check for changes in speed
-    var oldX = x;
-    var oldY = y;
-    var oldZ = z;
-    
-    var pub = true;
+  // Обработчик нажатий клавиш
+  var keys = {};
+  var handleKeyDown = function(e) { keys[e.keyCode] = true; }
+  var handleKeyUp   = function(e) { keys[e.keyCode] = false; }
 
-    var speed = 0;
-    // throttle the speed by the slider and throttle constant
-    if (keyDown === true) {
-      speed = throttle * that.scale;
-    }
-    // check which key was pressed
-    switch (keyCode) {
-      case 65:
-        // turn left
-        z = 1.5 * speed;
-        break;
-      case 87:
-        // up
-        x = 0.2 * speed;
-        break;
-      case 68:
-        // turn right
-        z = -1.5 * speed;
-        break;
-      case 83:
-        // down
-        x = -0.2 * speed;
-        break;
-      case 69:
-        // strafe right
-        y = -0.5 * speed;
-        break;
-      case 81:
-        // strafe left
-        y = 0.5 * speed;
-        break;
-      default:
-        pub = false;
-    }
+  document.body.addEventListener('keydown', handleKeyDown, false);
+  document.body.addEventListener('keyup', handleKeyUp, false);
 
-    // publish the command
-    if (pub === true) {
-      var twist = new ROSLIB.Message({
-        angular : {
-          x : 0,
-          y : 0,
-          z : z
-        },
-        linear : {
-          x : x,
-          y : y,
-          z : z
-        }
-      });
-      cmdVel.publish(twist);
+  // Функция обновления команд движения
+  var updateCmd = function() {
+    var speed = throttle * that.scale;
+    x = 0; y = 0; z = 0;
 
-      // check for changes
-      if (oldX !== x || oldY !== y || oldZ !== z) {
-        that.emit('change', twist);
-      }
-    }
+    if(keys[87]) { x = 0.8 * speed; }     // W - вперед
+    if(keys[83]) { x = -0.8 * speed; }    // S - назад
+    if(keys[65]) { z = 1.5 * speed; }     // A - влево
+    if(keys[68]) { z = -1.5 * speed; }    // D - вправо
   };
 
-  // handle the key
-  var body = document.getElementsByTagName('body')[0];
-  body.addEventListener('keydown', function(e) {
-    handleKey(e.keyCode, true);
-  }, false);
-  body.addEventListener('keyup', function(e) {
-    handleKey(e.keyCode, false);
-  }, false);
+  // Таймер публикации 200 Гц (каждые 5 мс)
+  setInterval(function() {
+    updateCmd();
+    var twist = new ROSLIB.Message({
+      linear:  { x:x, y:y, z:0 },
+      angular: { x:0, y:0, z:z }
+    });
+    cmdVel.publish(twist);
+  }, 5);
 };
+
 KEYBOARDTELEOP.Teleop.prototype.__proto__ = EventEmitter2.prototype;
